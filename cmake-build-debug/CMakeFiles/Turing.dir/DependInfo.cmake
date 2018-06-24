cdf15afeb45345b3d5e0f1aaaf4f241bcfb561ee
Joufulwarden <40517975+Joufulwarden@users.noreply.github.com>
2018-06-24 19:17:12 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Turing.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/user/CLionProjects/TuringCmdMake/TuringAlgorithm.c" "C:/Users/user/CLionProjects/TuringCmdMake/cmake-build-debug/CMakeFiles/Turing.dir/TuringAlgorithm.c.obj"
+  "C:/Users/user/CLionProjects/TuringCmdMake/inputFile.c" "C:/Users/user/CLionProjects/TuringCmdMake/cmake-build-debug/CMakeFiles/Turing.dir/inputFile.c.obj"
+  "C:/Users/user/CLionProjects/TuringCmdMake/inputTapes.c" "C:/Users/user/CLionProjects/TuringCmdMake/cmake-build-debug/CMakeFiles/Turing.dir/inputTapes.c.obj"
+  "C:/Users/user/CLionProjects/TuringCmdMake/main.c" "C:/Users/user/CLionProjects/TuringCmdMake/cmake-build-debug/CMakeFiles/Turing.dir/main.c.obj"
+  "C:/Users/user/CLionProjects/TuringCmdMake/outputFile.c" "C:/Users/user/CLionProjects/TuringCmdMake/cmake-build-debug/CMakeFiles/Turing.dir/outputFile.c.obj"
+  "C:/Users/user/CLionProjects/TuringCmdMake/printHelps.c" "C:/Users/user/CLionProjects/TuringCmdMake/cmake-build-debug/CMakeFiles/Turing.dir/printHelps.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Turing.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Turing.dir/main.c.obj"
+  "CMakeFiles/Turing.dir/inputFile.c.obj"
+  "CMakeFiles/Turing.dir/inputTapes.c.obj"
+  "CMakeFiles/Turing.dir/outputFile.c.obj"
+  "CMakeFiles/Turing.dir/printHelps.c.obj"
+  "CMakeFiles/Turing.dir/TuringAlgorithm.c.obj"
+  "Turing.pdb"
+  "Turing.exe"
+  "Turing.exe.manifest"
+  "libTuring.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Turing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
